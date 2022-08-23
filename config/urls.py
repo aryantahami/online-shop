a1f101ba4d91e10f1c0071cba6cfff2db9d3b60d
aryan <aryantahami@gmail.com>
2022-08-23 18:46:30 +0430

    improve product model

--- config/urls.py
+++ config/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     # path('accounts/', include('django.contrib.auth.urls')),
     # path('accounts/', include('accounts.urls')),
     path('accounts/', include('allauth.urls')),
+    path('products/',include('products.urls')),
 ]
---
+++ products/views.py
@@ -0,0 +1,8 @@
+from django.views import generic
+
+from .models import Product
+
+class ProductListView(generic.ListView):
+    model = Product
+    template_name = 'products/product_list.html'
+    context_object_name = 'products'
