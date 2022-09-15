65f6947668ae972117859cf78894d54d992864ca
aryan <aryancomp0106@gmail.com>
2022-09-15 17:33:30 +0430

    active comment and custom manager

--- products/models.py
+++ products/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 from django.shortcuts import reverse
+from django.contrib.auth import get_user_model
 
 
 class Product(models.Model):
@@ -18,3 +19,33 @@ class Product(models.Model):
     def get_absolute_url(self):
         return reverse('product_detail', args=[self.pk])
 
+
+class ActiveCommentsManager(models.Manager):
+    def get_queryset(self):
+        return super(ActiveCommentsManager, self).get_queryset().filter(active=True)
+
+
+class Comment(models.Model):
+    PRODUCT_STARS = [
+        ('1', 'very bad'),
+        ('2', 'bad'),
+        ('3', 'normal'),
+        ('4', 'good'),
+        ('5', 'perfect'),
+    ]
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comment', )
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='comment', )
+    body = models.TextField()
+    stars = models.CharField(max_length=10, choices=PRODUCT_STARS)
+
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
+
+    active = models.BooleanField(default=True)
+
+    # manager
+    objects = models.Manager()
+    active_comment_manager = ActiveCommentsManager()
+
+    def get_absolute_url(self):
+        return reverse('product_detail', args=[self.product.id])
